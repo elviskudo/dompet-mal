39d85514fe257cb052b50139049518fc3ebf856a
faizzulkarnaiin <faizmaqusa7@gmail.com>
2025-01-08 16:45:45 +0700

    update halaman home

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,9 @@
+import 'package:dompet_mal/component/bannerCategoryChoice.dart';
+import 'package:dompet_mal/component/bannerOperatonalFunds.dart';
+import 'package:dompet_mal/component/bannerSliderMorningCharity.dart';
+import 'package:dompet_mal/component/search.dart';
+import 'package:dompet_mal/models/MorningCharity.dart';
+import 'package:dompet_mal/models/pilihanKategoriModel.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -13,11 +19,32 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body:  ListView(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.only(top: 50, left: 16, right: 16, bottom: 36),
+            child: Column(
+              children: [
+                SearchBars(controller: controller.searchController),
+                SizedBox(
+                  height: 16,
+                ),
+                BannerSlider(
+                  banners: dummyMorningCharity,
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                BannerDanaOperasional(),
+                SizedBox(
+                  height: 18,
+                ),
+                BannerKategori(banners: dummyDataListCategoryBanner,)
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
