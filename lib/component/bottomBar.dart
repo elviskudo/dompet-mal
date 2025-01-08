138c1639832af225fdeabba51addab058ea76805
Rayyasa <rayya.a192018@gmail.com>
2025-01-08 09:58:01 +0700

    add folder assets

---
+++ lib/component/bottomBar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class bottomBar  extends StatelessWidget {
+  const bottomBar ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
