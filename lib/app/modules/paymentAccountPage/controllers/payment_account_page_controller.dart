8731dffed5123c781a6d47aac575b5b71a48babd
faizzulkarnaiin <faizmaqusa7@gmail.com>
2025-01-09 17:01:04 +0700

    ok

--- lib/app/modules/paymentAccountPage/controllers/payment_account_page_controller.dart
+++ lib/app/modules/paymentAccountPage/controllers/payment_account_page_controller.dart
@@ -1,7 +1,31 @@
+import 'package:dompet_mal/models/BankAccountModel.dart';
 import 'package:get/get.dart';
 
 class PaymentAccountPageController extends GetxController {
   //TODO: Implement PaymentAccountPageController
+  final bankAccounts = <BankAccount>[
+    BankAccount(
+      id: 1,
+      uuid: "",
+      name: "BANK MANDIRI",
+      accountName: "ELVIS SONATHA",
+      accountNumber: "1670003807988",
+      status: 1,
+      createdAt: "",
+      updatedAt: "",
+    ),
+    // Add more bank accounts as needed
+  ].obs;
+
+  final selectedBank = Rx<BankAccount?>(null);
+
+  void selectBankAccount(BankAccount account) {
+    selectedBank.value = account;
+    print('adad${account}');
+    print('adad${account.accountName}');
+    print('adadadad${selectedBank.value}');
+    Get.back(result: account); 
+  }
 
   final count = 0.obs;
   @override
